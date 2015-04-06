e83376adc3b78eeeff3d62e66b1d01f580608ca7
Giannis Neokleous <giannis@knewton.com>
2015-04-06 13:20:25 -0400

    Updating the run script with more options
    - Updating the run script with more options
    
    - Adding jar in dockerfile

--- collector/run.sh
+++ collector/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 scala \
-    -classpath ./tdist-zipkin/target/tdist-zipkin.jar com.knewton.tdist.zipkin.receiver.kafka.KafkaReceiverApp\
+    -classpath /tdist-zipkin.jar com.knewton.tdist.zipkin.receiver.kafka.KafkaReceiverApp\
     -zipkin.itemQueue.maxSize=10\
     -com.twitter.finagle.tracing.debugTrace=true\
-    -zipkin.kafka.groupid=0
+    -zipkin.kafka.groupid=0\
+    -zipkin.zookeeper.location=host1:2181,host2:2181,host3:2181/Kafka08\
+    -zipkin.kafka.server=host1:9160,host2:9160,host3:9160\
+    -zipkin.kafka.topics=zipkin-tdist=1
