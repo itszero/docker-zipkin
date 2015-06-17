172d0e54cc0a7f81fa533a6918891e991cc4a298
Sarah Haskins <sarah@knewton.com>
2015-06-17 18:53:12 -0400

    Changes to support new kinesis-collector

--- collector/run.sh
+++ collector/run.sh
@@ -8,7 +8,7 @@ scala \
     -zipkin.itemQueue.maxSize=10\
     -com.twitter.finagle.tracing.debugTrace=false\
     -zipkin.kafka.groupid=collector\
-    -zipkin.kafka.server=host1:2181,host2:2181,host3:2181/Kafka08\
+    -zipkin.kafka.server=ec2-174-129-254-64.compute-1.amazonaws.com:2181,ec2-23-21-69-105.compute-1.amazonaws.com:2181,ec2-75-101-130-163.compute-1.amazonaws.com:2181/Kafka08\
     -zipkin.kafka.topics=zipkin=1\
     -zipkin.store.cassie.dest=$DB_PORT_9160_TCP_ADDR:9160\
     -com.twitter.finagle.zipkin.initialSampleRate=1.0\
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ ROOT_URL="http://deb.local:$PUBLIC_PORT"
 name=$1
 cleanup=$2
 if [[ $cleanup == "y" ]]; then
-  # SERVICES=("cassandra" "collector" "query" "web")
+  # SERVICES=("cassandra" "collector" "kinesis-collector" "query" "web")
   SERVICES=("$name")
   for i in "${SERVICES[@]}"; do
     echo "** Stopping zipkin-$i"
@@ -23,11 +23,13 @@ if [ $name == "cassandra" ]; then
 elif [ $name == "collector" ]; then
 	echo "** Starting zipkin-collector"
 	docker run -i --link="${NAME_PREFIX}cassandra:db" -p 9410:9410 -p 9903:9903 --name="${NAME_PREFIX}collector" -t "${IMG_PREFIX}collector"
+elif [ $name == "kinesis-collector" ]; then
+	echo "** Starting zipkin-kinesis-collector"
+	docker run -i --link="${NAME_PREFIX}cassandra:db" -p 9410:9410 -p 9903:9903 --name="${NAME_PREFIX}kinesis-collector" -t "${IMG_PREFIX}kinesis-collector"
 elif [ $name == "query" ]; then
 	echo "** Starting zipkin-query"
-	docker run -i --link="${NAME_PREFIX}cassandra:db" -p 9411:9411 --name="${NAME_PREFIX}query" -t "${IMG_PREFIX}query" 
+	docker run -i --link="${NAME_PREFIX}cassandra:db" -p 9411:9411 --name="${NAME_PREFIX}query" -t "${IMG_PREFIX}query"
 elif [ $name == "web" ]; then
 	echo "** Starting zipkin-web"
-	docker run -i --link="${NAME_PREFIX}query:query" -p 8080:$PUBLIC_PORT -e "ROOTURL=${ROOT_URL}" --name="${NAME_PREFIX}web" -t "${IMG_PREFIX}web" 
+	docker run -i --link="${NAME_PREFIX}query:query" -p 8080:$PUBLIC_PORT -e "ROOTURL=${ROOT_URL}" --name="${NAME_PREFIX}web" -t "${IMG_PREFIX}web"
 fi
-
