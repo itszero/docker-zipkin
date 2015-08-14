355d3445c3a413de562a9c6a17105e6a1e39207a
Kristof Adriaenssens <kristof.adriaenssens@soundcloud.com>
2015-08-14 16:51:50 +0200

    Update query config file
    Update query config file to use new
    Datastax Cassandra driver.

--- query/run.sh
+++ query/run.sh
@@ -10,18 +10,20 @@ SERVICE_NAME="zipkin-query-service"
 CONFIG="${SERVICE_NAME}/config/query-cassandra.scala"
 
 cat << EOF > $CONFIG
+import com.datastax.driver.core.Cluster
+import com.datastax.driver.core.SocketOptions
 import com.twitter.zipkin.builder.QueryServiceBuilder
 import com.twitter.zipkin.cassandra
 import com.twitter.zipkin.storage.Store
+import org.twitter.zipkin.storage.cassandra.ZipkinRetryPolicy
 
-// development mode.
-val keyspaceBuilder = cassandra.Keyspace.static(nodes = Set("${DB_PORT_7000_TCP_ADDR}"))
-val storeBuilder = Store.Builder(
-  cassandra.StorageBuilder(keyspaceBuilder),
-  cassandra.IndexBuilder(keyspaceBuilder),
-  cassandra.AggregatesBuilder(keyspaceBuilder))
+val cluster = Cluster.builder()
+  .addContactPoints("${DB_PORT_9042_TCP_ADDR}")
+  .withSocketOptions(new SocketOptions().setConnectTimeoutMillis(10000).setReadTimeoutMillis(20000))
+  .withRetryPolicy(ZipkinRetryPolicy.INSTANCE)
+  .build()
 
-QueryServiceBuilder(storeBuilder)
+val storeBuilder = Store.Builder(new cassandra.SpanStoreBuilder(cluster))
 EOF
 
 echo "** Starting ${SERVICE_NAME}..."
