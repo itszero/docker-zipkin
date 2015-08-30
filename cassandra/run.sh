6951bfcb08e62e4bb896e833e813f3193b730c6c
Zoltan Nagy <abesto@abesto.net>
2015-08-30 18:57:05 +0200

    Use a minimal base image

--- cassandra/run.sh
+++ cassandra/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-IP=`hostname --ip-address`
-CONFIG_TMPL="/etc/cassandra/cassandra.default.yaml"
-CONFIG="/etc/cassandra/cassandra.yaml"
-rm $CONFIG; cp $CONFIG_TMPL $CONFIG
+
+set -eu
+
+IP=`hostname -i`
+CONFIG_TMPL="/cassandra/conf/cassandra.default.yaml"
+CONFIG="/cassandra/conf/cassandra.yaml"
+rm -f $CONFIG; cp $CONFIG_TMPL $CONFIG
 sed -i -e "s/^listen_address.*/listen_address: $IP/" $CONFIG
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/" $CONFIG
 sed -i -e "s/^\# broadcast_rpc_address.*/broadcast_rpc_address: $IP/" $CONFIG
-/usr/sbin/cassandra -f
+/cassandra/bin/cassandra -f
