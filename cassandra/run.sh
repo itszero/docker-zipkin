0569cd994cd27dd9f297d671c757f4f6ceb01fd6
Matthew Clarke <matthew.clarke@ft.com>
2017-12-06 16:27:44 +0000

    add comments

--- cassandra/run.sh
+++ cassandra/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# If the schema has been removed due to mounting, restore from our backup. see: install.sh
 if [ ! -d "/cassandra/data/data/zipkin2" ]; then
     cp -rf /cassandra/data-backup/* /cassandra/data/
 fi
