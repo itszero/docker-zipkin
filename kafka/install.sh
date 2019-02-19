ea5e3ecbf3ebd4bd3881279c8b4deec659e09007
Anuraag Agrawal <rag@istellar.jp>
2019-02-19 19:00:27 +0900

    Migrate non-mysql, kafka doesn't work yet.

--- kafka/install.sh
+++ kafka/install.sh
@@ -2,7 +2,7 @@
 set -eux
 
 echo "*** Installing Kafka and dependencies"
-apk add --update --no-cache runit jq
+apt-get -y update && apt-get -y install runit jq curl
 
 # download and cherry-pick zookeeper binaries
 APACHE_MIRROR=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
@@ -15,7 +15,7 @@ mv zookeeper-$ZOOKEEPER_VERSION/conf zookeeper/
 # create runit config
 mkdir -p /etc/service/zookeeper
 cat > /etc/service/zookeeper/run <<-EOF
-#!/bin/sh
+#!/busybox/sh
 exec java -Dzookeeper.log.dir=/kafka/zookeeper -Dzookeeper.root.logger=INFO,CONSOLE -cp /kafka/zookeeper/lib/*:/kafka/zookeeper/conf org.apache.zookeeper.server.quorum.QuorumPeerMain /kafka/zookeeper/conf/zoo_sample.cfg
 EOF
 chmod +x /etc/service/zookeeper/run
@@ -39,7 +39,7 @@ EOF
 # create runit config, dependent on zookeeper, that advertises the container ip
 mkdir -p /etc/service/kafka
 cat > /etc/service/kafka/run <<-"EOF"
-#!/bin/sh
+#!/busybox/sh
 sv start zookeeper || exit 1
 if [[ -z "$KAFKA_ADVERTISED_HOST_NAME" ]]; then
 listeners=PLAINTEXT://:9092
@@ -49,7 +49,7 @@ else
   # Have internal docker producers and consumers use the normal hostname:9092, and outside docker the advertised host on port 19092
   echo "advertised.listeners=PLAINTEXT://${HOSTNAME}:9092,PLAINTEXT_HOST://${KAFKA_ADVERTISED_HOST_NAME}:19092" >> /kafka/config/server.properties
 fi
-exec sh /kafka/bin/kafka-run-class.sh -name kafkaServer -loggc kafka.Kafka /kafka/config/server.properties
+exec sh /kafka/bin/kafka-run-class.sh -name kafkaServer kafka.Kafka /kafka/config/server.properties
 EOF
 chmod +x /etc/service/kafka/run
 
